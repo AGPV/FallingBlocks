092980bbeaecf5b5bc60c01a7661bf0d741e6e8a
Agapov Nikita <nikitaagapov2001@gmail.com>
2020-04-10 22:11:55 +0300

    trying add queue

--- FallingBlocks/src/FallingBlocks.c
+++ FallingBlocks/src/FallingBlocks.c
@@ -17,7 +17,10 @@ const int SCREEN_HEIGHT = 480;
 const int BLOCK_WIDTH = 160;
 const int BLOCK_HEIGHT = 64;
 
-uint8_t greyLevel = 0x0F;
+typedef struct node {
+    SDL_Rect block;
+    struct node * next;
+} tower;
 
 
 
@@ -38,14 +41,18 @@ int main(int argc, char *argv[]) {
 
 
 
-            SDL_Rect rect;
-            rect.h = BLOCK_HEIGHT;
-            rect.w = BLOCK_WIDTH;
-            rect.x = SCREEN_WIDTH/2 - BLOCK_WIDTH/2;
-            rect.y = 0;
 
 
-
+            tower * head = NULL;
+            head = malloc(sizeof(tower));
+            if (head == NULL){
+                return 1;
+            }
+            head->block.h = BLOCK_HEIGHT;
+            head->block.w = BLOCK_WIDTH;
+            head->block.x = SCREEN_WIDTH/2 - BLOCK_WIDTH/2;
+            head->block.y = 0;
+            head->next = NULL;
 
             int quit = 0;
             char motionFlag = 'r';
@@ -58,17 +65,17 @@ int main(int argc, char *argv[]) {
                                 else if (event.type == SDL_KEYDOWN) {
                                     switch (event.key.keysym.sym) {
                                     case SDLK_SPACE:
-                                        while(rect.y+rect.h < SCREEN_HEIGHT){
+                                        while(head->block.y+head->block.h < SCREEN_HEIGHT){
                                             motionFlag = 'd';
-                                            rect.y += 10;
+                                            head->block.y += 10;
                                             SDL_FillRect(screenSurface, NULL,
                                                     SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-                                            SDL_FillRect(screenSurface, &rect,
+                                            SDL_FillRect(screenSurface, &head->block,
                                                                         SDL_MapRGB(screenSurface->format, 0, 0, 0));
                                             SDL_Delay(5);
                                             SDL_UpdateWindowSurface(window);
                                         }
-                                        motionFlag = 'r';
+                                        motionFlag = 's';
                                         break;
                                     case SDLK_ESCAPE:
                                         quit = 1;
@@ -76,26 +83,26 @@ int main(int argc, char *argv[]) {
                                     }
                                 }
                             }
-                    if(rect.x + rect.w <SCREEN_WIDTH && motionFlag =='r'){
+                    if(head->block.x + head->block.w <SCREEN_WIDTH && motionFlag =='r'){
                     SDL_FillRect(screenSurface, NULL,
                             SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-                    SDL_FillRect(screenSurface, &rect,
+                    SDL_FillRect(screenSurface, &head->block,
                             SDL_MapRGB(screenSurface->format, 0, 0, 0));
-                    rect.x ++;
-                    SDL_Delay(5);
+                    head->block.x ++;
+                    SDL_Delay(1);
                     SDL_UpdateWindowSurface(window);
-                } else {
-                    motionFlag = 'l';
-                    if (rect.x>=0 && motionFlag == 'l'){
-                    SDL_FillRect(screenSurface, NULL,
-                            SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-                    SDL_FillRect(screenSurface, &rect,
-                            SDL_MapRGB(screenSurface->format, 0, 0, 0));
-                    rect.x --;
-                    SDL_Delay(5);
-                    SDL_UpdateWindowSurface(window);
-                    if (rect.x == 0) {
-                        motionFlag = 'r';
+                    } else if(motionFlag!='s'){
+                        motionFlag = 'l';
+                        if (head->block.x>=0 && motionFlag == 'l'){
+                            SDL_FillRect(screenSurface, NULL,
+                                    SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+                            SDL_FillRect(screenSurface, &head->block,
+                                    SDL_MapRGB(screenSurface->format, 0, 0, 0));
+                            head->block.x --;
+                            SDL_Delay(1);
+                            SDL_UpdateWindowSurface(window);
+                            if (head->block.x == 0) {
+                            motionFlag = 'r';
                     }
                     }
                 }
